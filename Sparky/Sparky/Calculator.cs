80e28451998b5e18b88959890f672ee963409f72
sagor31h2 <sagor31h2@gmail.com>
2022-07-01 18:38:26 +0600

    Checking multple value

--- Sparky/Sparky/Calculator.cs
+++ Sparky/Sparky/Calculator.cs
@@ -1,10 +1,18 @@
 ﻿namespace Sparky
 {
     public class Calculator
-    {
+    {   //Adding two numbers
         public int AddNumbers(int a, int b)
         {
             return a + b;
         }
+
+        //Cheaking isOdd
+        public bool IsOdd(int a)
+        {
+            return (a % 2 != 0);
+        }
     }
+
+
 }
--- Sparky/SparkyNUnitTest/CalculatorNUnitTests.cs
+++ Sparky/SparkyNUnitTest/CalculatorNUnitTests.cs
@@ -16,5 +16,41 @@ namespace Sparky
             //Assert
             Assert.AreEqual(11, result);
         }
+
+        [Test]
+        public void IsOdd_InputOddNumber_GetTrue()
+        {
+            var calculator = new Calculator();
+
+            var result = calculator.IsOdd(5);
+
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void IsOdd_InputEvenNumber_GetFalse()
+        {
+            var calculator = new Calculator();
+
+            var result = calculator.IsOdd(6);
+
+            Assert.That(result, Is.EqualTo(false));
+        }
+
+        //passing multiple value
+        [Test]
+        [TestCase(2)]
+        [TestCase(12)]
+        [TestCase(114)]
+        [TestCase(126)]
+
+        public void IsOdd_InputMultiplaEvenNumber_GetFalse(int a)
+        {
+            var calculator = new Calculator();
+
+            var result = calculator.IsOdd(a);
+
+            Assert.That(result, Is.EqualTo(false));
+        }
     }
 }
