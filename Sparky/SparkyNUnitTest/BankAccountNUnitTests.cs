4b170b90dc4f9b0b17b230366555319b02f7c742
sagor31h2 <sagor31h2@gmail.com>
2022-07-19 16:38:07 +0600

    Add moq

--- Sparky/SparkyNUnitTest/BankAccountNUnitTests.cs
+++ Sparky/SparkyNUnitTest/BankAccountNUnitTests.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Moq;
 using NUnit.Framework;
 
 namespace Sparky
@@ -9,17 +10,27 @@ namespace Sparky
     [TestFixture]
     public class BankAccountNUnitTests
     {
-        private BankAccount _bankAccount;
+        private BankAccount Account;
         [SetUp]
         public void SetUp()
         {
-            _bankAccount = new BankAccount(new LogBook());
         }
 
         //deposite 100
         [Test]
+        public void BankDepositeLogFaker_Add100_ReturnTrue()
+        {
+            var _bankAccount = new BankAccount(new LogBook());
+            var result = _bankAccount.Deposit(100);
+            Assert.IsTrue(result);
+            Assert.AreEqual(_bankAccount.Getbalance(), 100);
+        }
+
+        [Test]
         public void BankDeposite_Add100_ReturnTrue()
         {
+            var logMock = new Mock<ILogBook>();
+            var _bankAccount = new BankAccount(logMock.Object);
             var result = _bankAccount.Deposit(100);
             Assert.IsTrue(result);
             Assert.AreEqual(_bankAccount.Getbalance(), 100);
